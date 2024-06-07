568f0431211e8e80733632af6ae261cdbc7d7f54
kang <andrew4162@snu.ac.kr>
2024-06-07 13:49:33 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -4,15 +4,25 @@
 using namespace std;
 
 int N,M;
-vector<int> vec;
+vector<int> vec(9);
+vector<bool> visited(9,false); 
+
 
 void func(int cnt) {
-
-
-
-
-
-
+    if(cnt==M) {
+        for(auto i =0;i<M;i++)
+            cout << vec[i] << " ";
+        cout << "\n";
+    }else {
+        for(auto i=1; i<=N;i++) {
+            if(!visited[i]) {
+                visited[i]=true;
+                vec[cnt]=i; 
+                func(cnt+1); 
+                visited[i]=false;
+            }
+        }
+    }
 
 }
 
